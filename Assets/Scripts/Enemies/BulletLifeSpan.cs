226f5e39a93d399f8538a3788033569e90347d6a
AndyCQ <acq3433@nyu.edu>
2023-11-11 01:25:03 -0500

    Stop bullets from going through walls

--- Assets/Scripts/Enemies/BulletLifeSpan.cs
+++ Assets/Scripts/Enemies/BulletLifeSpan.cs
@@ -10,11 +10,25 @@ public class BulletLifeSpan : MonoBehaviour
         Destroy(gameObject, lifeTime);
     }
 
+
     private void OnCollisionEnter2D(Collision2D other) {
         Destroy(gameObject, 1f);
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        Destroy(gameObject, 1f);
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            // Bullet collided with ground, handle the impact
+            HandleGroundCollision();
+        }
+        
     }
+
+    private void HandleGroundCollision()
+    {
+        // Add any logic here for what should happen when the bullet hits the ground
+        // For example, destroy the bullet GameObject
+        Destroy(gameObject);
+    }
+
 }
